5c89ada7ccf77efb040a2ee6d3c03c6b1a12987a
AntoniosValais <toniovkozz@gmail.com>
2015-11-10 03:53:39 +0200

    forgot to add test files

---
+++ SnapChaty/src/test/java/gr/teicm/toulou/websocketservlet/test/WebSocketServletTest.java
@@ -0,0 +1,55 @@
+package gr.teicm.toulou.websocketservlet.test;
+
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/*
+*
+* @Author AntoniosValais
+*
+*/
+
+public class WebSocketServletTest
+{
+	private WebSocketClientMock webSocketClientMock;
+	
+	@Before
+	public void beforeTest()
+	{
+		
+	}
+	
+	@Test
+	public void connectionEstablishTest()
+	{
+		webSocketClientMock = new WebSocketClientMock();
+		
+		String sessionId = webSocketClientMock.createConnection();
+		
+		Boolean sessionClosed = webSocketClientMock.closeConnection();
+		
+		assertNotNull( sessionId );
+		assertTrue( sessionClosed );
+	}
+	
+	@Test
+	public void servletReceiveMessageTest()
+	{
+		webSocketClientMock = new WebSocketClientMock();
+		
+		String sessionId = webSocketClientMock.createConnection();
+		
+		Boolean sendMessageDone = webSocketClientMock.sendMessage( "dada!" );
+		
+		Boolean sessionClosed = webSocketClientMock.closeConnection();
+		
+		assertNotNull( sessionId );
+		assertTrue( sessionClosed );
+		assertTrue( sendMessageDone );	
+	}
+	
+	
+	
+}
